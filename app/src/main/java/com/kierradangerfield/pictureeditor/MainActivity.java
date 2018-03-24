d0570ad9188d0d6c78134117607628da546f809c
DangerRanger7 <kierradachelle@yahoo.com>
2018-03-24 09:44:46 -0500

    added button

--- app/src/main/java/com/kierradangerfield/pictureeditor/MainActivity.java
+++ app/src/main/java/com/kierradangerfield/pictureeditor/MainActivity.java
@@ -2,6 +2,7 @@ package com.kierradangerfield.pictureeditor;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +10,13 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //open gallery
+        findViewById(R.id.openGallery_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                
+            }
+        });
     }
 }
