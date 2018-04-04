494448f01268a861449b1e77562239c9268e86be
DangerRanger7 <kierradachelle@yahoo.com>
2018-04-04 10:50:36 -0500

    able to draw lines

--- app/src/main/java/com/kierradangerfield/pictureeditor/MainActivity.java
+++ app/src/main/java/com/kierradangerfield/pictureeditor/MainActivity.java
@@ -10,7 +10,10 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.constraint.solver.widgets.Rectangle;
+import android.view.Display;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageView;
@@ -21,16 +24,33 @@ import java.net.URI;
 
 public class MainActivity extends Activity {
 private int reqCode = 1;
-static final int TAKE_PICTURE = 1;
+static final int TAKE_PICTURE = 2;
 ImageView imageView;
+
+    private static int IMAGE = 1;
     private Bitmap bitmap;
     private Canvas canvas;
     private Paint linePaint;
-String path;
+
+    float xDown = 0, yDown = 0;
+    float xUp, yUp;
+
+    //String path;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //DrawView drawView = new DrawView(this);
+
+
+      /*findViewById(R.id.rectangle_button).setOnClickListener(new View.OnClickListener() {
+          @Override
+          public void onClick(View view) {
+              DrawView dv = findViewById(R.id.drawView);
+              dv.addRectangle();
+          }
+      });*/
 
         //open gallery
         findViewById(R.id.openGallery_button).setOnClickListener(new View.OnClickListener() {
@@ -41,7 +61,18 @@ String path;
         });
 
                 //image
-        findViewById(R.id.picture_imageView).setOnTouchListener(new View.OnTouchListener() {
+        this.findViewById(R.id.picture_imageView).setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                DrawView dv = findViewById(R.id.drawView);
+                dv.setup();
+
+                return false;
+            }
+        });
+
+
+       /* findViewById(R.id.picture_imageView).setOnTouchListener(new View.OnTouchListener() {
                     @Override
                     public boolean onTouch(View view, MotionEvent motionEvent) {
 
@@ -69,28 +100,20 @@ String path;
                         }
                         return false;
                     }
-                });
+                });*/
 
         /*
         * source
         * https://stackoverflow.com/questions/15704205/how-to-draw-line-on-imageview-along-with-finger-in-android*/
 
-        //edit
+        //edit***************************************************************
         findViewById(R.id.edit_button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-            //ImageView imageView =  findViewById(R.id.picture_imageView);
-                bitmap = Bitmap.createBitmap((int) imageView.getWidth(), (int) imageView.getHeight(), Bitmap.Config.ARGB_8888);
-                canvas = new Canvas(bitmap);
-               linePaint = new Paint();
-               linePaint.setColor(Color.BLACK);
-               linePaint.setStyle(Paint.Style.FILL);
-               //ImageView imageView = findViewById(R.id.drawView);
-               imageView.setImageBitmap(bitmap);
-               //imageView.setOnTouchListener(this);
-
+                //editButtonClicked();
             }
         });
+
 
         /****************** SAVE BUTTON***************************************************************/
         findViewById(R.id.save_button).setOnClickListener(new View.OnClickListener() {
@@ -109,13 +132,34 @@ String path;
         findViewById(R.id.takePicture_button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
                 Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
                 if (intent.resolveActivity(getPackageManager()) != null){
                     startActivityForResult(intent, TAKE_PICTURE);
                 }
             }
         });
+
     }
+
+    public void editButtonClicked(){
+        //displays width and height
+        Display display = getWindowManager().getDefaultDisplay();
+
+       float displayWidth = display.getWidth();
+        float displayHeight = display.getHeight();
+
+        //ImageView imageView = findViewById(R.id.picture_imageView);
+        bitmap = Bitmap.createBitmap((int) displayWidth, (int) displayHeight, Bitmap.Config.ARGB_8888);
+
+        canvas = new Canvas(bitmap);
+        linePaint = new Paint();
+        linePaint.setColor(Color.CYAN);
+        imageView.setImageBitmap(bitmap);
+        imageView.setOnTouchListener((View.OnTouchListener) this);
+    }
+
 
 
 
@@ -134,8 +178,9 @@ String path;
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        try {
-
+        switch (requestCode) {
+            //try {
+            case 1:
             if (requestCode == reqCode && resultCode == RESULT_OK && null != data) {
                 Uri selectPic = data.getData();
                 String[] image = {MediaStore.Images.Media.DATA};
@@ -146,20 +191,129 @@ String path;
                 cursor.moveToFirst();
 
                 int columnIndex = cursor.getColumnIndex(image[0]);
-                 path = cursor.getString(columnIndex);
+                String path = cursor.getString(columnIndex);
                 cursor.close();
 
                 ImageView imageView = findViewById(R.id.picture_imageView);
                 //set image
                 imageView.setImageBitmap(BitmapFactory.decodeFile(path));
-            } else {
-                Toast.makeText(getApplicationContext(), "An image has not been picked!",
-                        Toast.LENGTH_LONG).show();
-            }
 
-        } catch (Exception e) {
+                DrawView drawView =findViewById(R.id.drawView);
+
+                //DrawView dv = findViewById(R.id.drawView);
+               // drawView.start();
+              //  imageView.setOnTouchListener((View.OnTouchListener) this);
+
+            }
+            break;
+
+            //save image
+            /*case 2:
+                // if (requestCode == reqCode && resultCode == RESULT_OK) {
+                Bundle extras = data.getExtras();
+                Bitmap imageBitmap = (Bitmap) extras.get("data");
+                ImageView iv = findViewById(R.id.picture_imageView);
+                iv.setImageBitmap(imageBitmap);
+              /*  } else {
+                    Toast.makeText(getApplicationContext(), "An image has not been picked!",
+                            Toast.LENGTH_LONG).show();
+                }*/
+                //break;
+
+            default:
+                break;
+       /* }catch (Exception e) {
             Toast.makeText(this, "Something went wrong", Toast.LENGTH_LONG).show();
+        }*/
+
+        }
+    }
+
+    /*@Override
+    public boolean onTouchEvent(MotionEvent event) {
+        int touch = event.getAction();
+
+        drawLines(imageView);
+        switch (touch){
+            case MotionEvent.ACTION_DOWN:
+                xDown = event.getX();
+                yDown = event.getY();
+                break;
+
+            case  MotionEvent.ACTION_MOVE:
+                break;
+
+            case MotionEvent.ACTION_UP:
+                xUp = event.getX();
+                yUp = event.getY();
+                canvas.drawLine(xDown, yDown, xUp, yUp, linePaint);
+                imageView.invalidate();
+                break;
+
+            default:
+                break;
         }
 
+        return super.onTouchEvent(event);
+
+    }*/
+
+    public void drawLines(ImageView imageView){
+        bitmap = Bitmap.createBitmap((int) imageView.getWidth(), (int) imageView.getHeight(), Bitmap.Config.ARGB_8888);
+        canvas = new Canvas(bitmap);
+        linePaint = new Paint();
+        linePaint.setColor(Color.BLACK);
+        linePaint.setStyle(Paint.Style.FILL);
+        //ImageView imageView = findViewById(R.id.drawView);
+        imageView.setImageBitmap(bitmap);
+
+      //  imageView.setOnTouchListener((View.OnTouchListener) this);
     }
+
+    /*@Override
+    public boolean onTouch(View view, MotionEvent motionEvent) {
+
+        int touch = motionEvent.getAction();
+
+        switch (touch){
+            case MotionEvent.ACTION_DOWN:
+                xDown = motionEvent.getX();
+                yDown = motionEvent.getY();
+                break;
+
+            case  MotionEvent.ACTION_MOVE:
+                break;
+
+            case MotionEvent.ACTION_UP:
+                xUp = motionEvent.getX();
+                yUp = motionEvent.getY();
+                canvas.drawLine(xDown, yDown, xUp, yUp, linePaint);
+                imageView.invalidate();
+                break;
+
+            default:
+                break;
+        }
+        return false;
+    }*/
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
+    }
+
+   /* @Override
+    protected void onResume() {
+        super.onResume();
+
+        DrawView dv = findViewById(R.id.drawView);
+        dv.setup();
+    }*/
+/* @Override
+    protected void onPause() {
+        super.onPause();
+        DrawView dv = findViewById(R.id.drawView);
+        dv.stop();
+
+    }*/
 }
