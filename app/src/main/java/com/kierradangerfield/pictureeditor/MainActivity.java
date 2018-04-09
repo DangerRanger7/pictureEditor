6b8d4aab7beaa6860b0a68752d4bcfbf68b4bb00
DangerRanger7 <kierradachelle@yahoo.com>
2018-04-09 16:15:09 -0500

    changes sizes of stroke

--- app/src/main/java/com/kierradangerfield/pictureeditor/MainActivity.java
+++ app/src/main/java/com/kierradangerfield/pictureeditor/MainActivity.java
@@ -1,6 +1,8 @@
 package com.kierradangerfield.pictureeditor;
 /*SOURCES: https://stackoverflow.com/questions/15704205/how-to-draw-line-on-imageview-along-with-finger-in-android*/
+
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
 import android.graphics.Bitmap;
@@ -9,19 +11,29 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.drawable.Drawable;
+import android.media.MediaScannerConnection;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.constraint.solver.widgets.Rectangle;
+import android.util.Log;
 import android.view.Display;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Toast;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.net.URI;
+import java.sql.Date;
+import java.text.SimpleDateFormat;
+import java.util.Random;
 
 public class MainActivity extends Activity {
     private int reqCode = 1;
@@ -36,7 +48,11 @@ public class MainActivity extends Activity {
     float xDown = 0, yDown = 0;
     float xUp, yUp;
 
-    //String path;
+    public static int tools = 1;
+   public static int size = 50;
+   public static int color = Color.BLACK;
+
+    String path;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -47,13 +63,87 @@ public class MainActivity extends Activity {
         final DrawView dv = findViewById(R.id.drawView);
         dv.setup();
 
- /*findViewById(R.id.rectangle_button).setOnClickListener(new View.OnClickListener() {
+ findViewById(R.id.rectangle_button).setOnClickListener(new View.OnClickListener() {
           @Override
           public void onClick(View view) {
-              RectangleView rv = findViewById(R.id.drawView);
-              rv.addRectangle();
+              DrawView dv = findViewById(R.id.drawView);
+              dv.rectangleSetUp();
           }
-      });*/
+      });
+
+        RadioButton rb = findViewById(R.id.rec_radioButton);
+
+        rb = findViewById(R.id.line_radioButton);
+        rb.setChecked(true);
+
+        //tools radio group
+        //RadioGroup radioGroup = findViewById(R.id.tools
+       RadioGroup rg =  findViewById(R.id.tools_radioGroup);
+       rg.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+           @Override
+           public void onCheckedChanged(RadioGroup radioGroup, int id) {
+               DrawView dv = findViewById(R.id.drawView);
+               if (id == R.id.rec_radioButton){
+                   tools = 0;
+                  // dv.drawRectangle();
+               }else if (id == R.id.line_radioButton){
+                   tools = 1;
+               }
+
+           }
+       });
+
+       findViewById(R.id.small_radioButton);
+       findViewById(R.id.Medium_radioButton);
+       findViewById(R.id.large_radioButton);
+
+     rg =  findViewById(R.id.size_radioGroup);
+     rg.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+         @Override
+         public void onCheckedChanged(RadioGroup radioGroup, int id) {
+            // RadioButton rb = id;
+             DrawView dv = findViewById(R.id.drawView);
+
+             if (id == R.id.small_radioButton){
+                 size = 25;
+             }else if (id == R.id.Medium_radioButton){
+                 size = 50;
+             }else if (id == R.id.large_radioButton){
+                 size = 75;
+             }
+
+             dv.changeSize(size);
+         }
+     });
+
+    RadioButton color_rb = findViewById(R.id.black_radioButton);
+    color_rb.setChecked(true);
+   // color_rb = findViewById(R.id.cyan_radioButton);
+        findViewById(R.id.cyan_radioButton);
+       // color_rb.setChecked(true);
+    findViewById(R.id.red_radioButton);
+
+    RadioGroup color_radioGroup = findViewById(R.id.color_radioGroup);
+    color_radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+        @Override
+        public void onCheckedChanged(RadioGroup radioGroup, int i) {
+           int selected = radioGroup.getCheckedRadioButtonId();
+           DrawView dv = findViewById(R.id.drawView);
+
+            if (selected == R.id.black_radioButton){
+
+                color = Color.BLACK;
+            }else if (selected == R.id.cyan_radioButton){
+                color = Color.CYAN;
+            }else if (selected == R.id.red_radioButton){
+                color = Color.RED;
+            }
+            dv.changeColor(color);
+        }
+    });
+
+
+
 
         //open gallery
         /*findViewById(R.id.openGallery_button).setOnClickListener(new View.OnClickListener() {
@@ -121,17 +211,15 @@ public class MainActivity extends Activity {
 
 
         /****************** SAVE BUTTON***************************************************************/
-      /*  findViewById(R.id.save_button).setOnClickListener(new View.OnClickListener() {
+       findViewById(R.id.save_button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-                /*Intent intent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
-                File file = new File(path);
-                Uri uri = Uri.fromFile(file);
-                intent.setData(uri);
-                startActivity(intent);/
+                DrawView dv = new DrawView(MainActivity.this);
+                dv.setDrawingCacheEnabled(true);
+                save();
             }
-        });*/
+        });
 
         /******TAKE PICTURE BUTTON*******************************************************************************************/
         findViewById(R.id.takePicture_button).setOnClickListener(new View.OnClickListener() {
@@ -148,7 +236,47 @@ public class MainActivity extends Activity {
 
     }
 
-    public void editButtonClicked() {
+
+
+public  void save() {
+    DrawView dv = new DrawView(this);
+    bitmap = dv.getDrawingCache();
+    try {
+        FileOutputStream fos = openFileOutput("name.png", Context.MODE_PRIVATE);
+        bitmap.compress(Bitmap.CompressFormat.PNG, 10, fos);
+
+        fos.close();
+    } catch (IOException e) {
+        e.printStackTrace();
+    }
+}
+
+
+
+    private File getMediaFile(){
+        File file = new File(Environment.getExternalStorageDirectory() +
+                            "/Android/data" +
+                            getApplicationContext().getPackageName() + "/Files");
+
+        if (!file.exists()){
+            if (!file.mkdirs()){
+                return null;
+            }
+        }
+
+
+        File mediaFile;
+        //String imageName = "MI_" +
+
+        return file;
+    }
+
+   /* public void onCheckedChanged(RadioGroup radioGroup, int clickedId){
+
+    }*/
+
+
+   /* public void editButtonClicked() {
         //displays width and height
         Display display = getWindowManager().getDefaultDisplay();
 
@@ -163,7 +291,10 @@ public class MainActivity extends Activity {
         linePaint.setColor(Color.CYAN);
         imageView.setImageBitmap(bitmap);
         imageView.setOnTouchListener((View.OnTouchListener) this);
-    }
+    }*/
+
+
+
 
 
 
@@ -178,7 +309,7 @@ public class MainActivity extends Activity {
         startActivityForResult(getPicIntent, reqCode );
     }*/
 /*SELECT IMAGE*/
-    @Override
+  /*  @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
@@ -223,17 +354,17 @@ public class MainActivity extends Activity {
               /*  } else {
                     Toast.makeText(getApplicationContext(), "An image has not been picked!",
                             Toast.LENGTH_LONG).show();
-                }*/
+                }*
                 //break;
 
             default:
                 break;
        /* }catch (Exception e) {
             Toast.makeText(this, "Something went wrong", Toast.LENGTH_LONG).show();
-        }*/
+        }*
 
         }
-    }
+    }*/
 
 
     /*@Override
@@ -265,7 +396,7 @@ public class MainActivity extends Activity {
 
     }*/
 
-    public void drawLines(ImageView imageView){
+    /*public void drawLines(ImageView imageView){
         bitmap = Bitmap.createBitmap((int) imageView.getWidth(), (int) imageView.getHeight(), Bitmap.Config.ARGB_8888);
         canvas = new Canvas(bitmap);
         linePaint = new Paint();
@@ -275,7 +406,7 @@ public class MainActivity extends Activity {
         imageView.setImageBitmap(bitmap);
 
       //  imageView.setOnTouchListener((View.OnTouchListener) this);
-    }
+    }*/
 
     /*@Override
     public boolean onTouch(View view, MotionEvent motionEvent) {
@@ -304,10 +435,6 @@ public class MainActivity extends Activity {
         return false;
     }*/
 
-    @Override
-    public void onPointerCaptureChanged(boolean hasCapture) {
-
-    }
 
    /* @Override
     protected void onResume() {
