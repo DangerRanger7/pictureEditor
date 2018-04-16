e178c8af24bfd78feb1aaf94b90f1a528dbb87d4
DangerRanger7 <kierradachelle@yahoo.com>
2018-04-16 14:40:22 -0500

    able to save somewhat

--- app/src/main/java/com/kierradangerfield/pictureeditor/DrawView.java
+++ app/src/main/java/com/kierradangerfield/pictureeditor/DrawView.java
@@ -5,6 +5,7 @@ package com.kierradangerfield.pictureeditor;
 * https://stackoverflow.com/questions/6956838/how-to-erase-previous-drawing-on-canvas*/
 
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
@@ -12,14 +13,18 @@ import android.graphics.Paint;
 import android.graphics.Path;
 import android.graphics.PorterDuff;
 import android.graphics.drawable.Drawable;
+import android.media.MediaScannerConnection;
+import android.net.Uri;
 import android.os.Environment;
 import android.support.constraint.solver.widgets.Rectangle;
 import android.util.AttributeSet;
 import android.util.DisplayMetrics;
+import android.util.Log;
 import android.util.TypedValue;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -33,38 +38,26 @@ import java.util.Random;
  */
 
 public class DrawView extends View {
-    int tool = MainActivity.tools;
 
-    /*private static int lineSize = 100;
-    private static int lineDIP = 5;*/
    int drawingSize; // = MainActivity.size;
     private static int androidDIP = 50;
 
-    private static int rectangleSizeDip = 50;
-    private static int rectangleStepDip = 5;
-
     private Paint linePaint;
     private Paint bitmapPaint;
-    private Random random;
 
     private int currentWidth;
     private int currentHeight;
 
     private Paint recPaint;
-    private ArrayList<Rectangle> rectangles;
-    private ArrayList<Path> paths = new ArrayList<Path>();
 
    public static Bitmap bitmap;
     private Canvas canvas;
     Path path;
 
     float x, y;
-    float width = 100.0f;
-    float height = 50.0f;
-
-    int color = MainActivity.color;
 
     int color2 = MainActivity.color;
+    int width = MainActivity.size;
 
   //  private ImageView imageView;
 
@@ -88,60 +81,27 @@ public class DrawView extends View {
     }
 
     public void setup(){
-      //  ImageView iv = findViewById(R.id.picture_imageView);
 
             linePaint = new Paint();
             linePaint.setAntiAlias(true);
             linePaint.setColor(color2);
             linePaint.setStyle(Paint.Style.STROKE);
+            linePaint.setStrokeWidth(width);
             drawingSize = 50;
 
             path = new Path();
-           /* bitmapPaint = new Paint();
-            bitmapPaint.setColor(Color.GREEN);*/
 
             loadBitmap();
-
-     /* /*RECTANGLE *
-        random = new Random();
-        recPaint = new Paint();
-        recPaint.setStyle(Paint.Style.FILL);
-        rectangles = new ArrayList<>();
-        setUpBitmap();*/
-       // setBackground(getBackground(R.id.picture_imageView));
-
-      /*  random = new Random();
-
-        linePaint = new Paint();
-        linePaint.setStyle(Paint.Style.FILL);
-
-        rectangles = new ArrayList<>();
-
-        setUpBitmap();*/
-      /*  linePaint = new Paint();
-        linePaint.setColor(Color.CYAN);
-        linePaint.setStyle(Paint.Style.STROKE);
-
-        int dpSize = 2; // want the line to be 2 dp wide
-        DisplayMetrics dm = getResources().getDisplayMetrics();
-        float strokeWidth = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dpSize, dm);
-        linePaint.setStrokeWidth(strokeWidth);
-        linePaint.setAntiAlias(true);*/
     }
 
     public void rectangleSetUp(){
-        /*recPaint = new Paint();
-        recPaint.setStyle(Paint.Style.FILL);
 
-        random = new Random();
-        rectangles = new ArrayList<>();
-
-        loadBitmap();*/
         x = y = 0;
 
         recPaint = new Paint();
         recPaint.setColor(color2);
         recPaint.setStyle(Paint.Style.STROKE);
+        recPaint.setStrokeWidth(width);
 
     }
 
@@ -153,20 +113,6 @@ public class DrawView extends View {
 
     }
 
-    private void setUpBitmap(){
-
-       // imageView = findViewById(R.id.picture_imageView);
-        DisplayMetrics dm = getResources().getDisplayMetrics();
-        int pixelSize = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, androidDIP, dm);
-
-
-
-        //make the drawable
-       /*Canvas canvas = new Canvas(bitmap);
-        imageView.setAdjustViewBounds(true);
-        imageView.getAdjustViewBounds();
-        imageView.draw(canvas);*/
-    }
 
     @Override
     protected void onSizeChanged(int w, int h, int oldw, int oldh) {
@@ -182,59 +128,13 @@ public class DrawView extends View {
 
     }
 
-    public void addRectangle(){
-        Rectangle r = new Rectangle(rectangleSizeDip, rectangleStepDip);
-        rectangles.add(r);
-    }
-
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
-
-        //while (tool == 1) {
-        //Path[] paths;
-        //linePaint.setColor(color);
-        //bitmapPaint.setColor(Color.WHITE);
-
         canvas.drawBitmap(bitmap, 0, 0, bitmapPaint);
+        canvas.drawPath(path, linePaint);
 
-            //if (MainActivity.recButton.isChecked()){
-
-              //  canvas.drawRect(downX, downY, x, y, recPaint);
-           // }else  {
-                canvas.drawPath(path, linePaint);
-            //}
-
-
-
-
-        //}
-
-       /* while (tool == 0){
-            canvas.drawRect(x,y,x + width, y + height, recPaint);
-        }*/
-        /*FOR RECTANGLE*/
-
-   /*  int size = rectangles.size();
-
-        for (int i = 0; i < size; i++){
-           Rectangle r = rectangles.get(i);
-
-
-            canvas.drawBitmap(bitmap, r.x, r.y, recPaint);
-        }*/
-
-
-       /* if (touch){
-            canvas.drawRect(x,y,x + width, y + height, recPaint);
-        }*/
-
-
-
-      //  canvas.drawPaint(backgroundPaint);
-      /*  canvas.drawLine(0, 0, currentWidth - 1, currentHeight - 1, linePaint);
-        canvas.drawLine(currentWidth - 1, 0, 0, currentHeight - 1, linePaint);*/
     }
 
     public void changeColor(int color){
@@ -254,22 +154,48 @@ public class DrawView extends View {
         invalidate();
     }
 
-    public void drawRectangle(){
-
-        /*while(tool == 0){
-            canvas.drawRect(x, y, x2, y2.getY(), drawPaint);
-        }*/
-        canvas.drawRect(downX, downY, x, y, recPaint);
-    }
-
     public void changeStyle(Paint.Style style){
         linePaint.setStyle(style);
         recPaint.setStyle(style);
     }
 
-    public void save(){
+    protected void save(Bitmap bitmap){
+        String root = Environment.getExternalStorageDirectory().toString();
+        File dir = new File(root + "saved_images");
+        dir.mkdirs();
+        Random r = new Random();
+        int n = 1000;
+        n = r.nextInt(n);
+        String fname = "Image-" + n + "jpg";
+        File file = new File(dir, fname);
+        if (file.exists()) {
+            file.delete();
+        }
+
+        Log.i("LOAD", root + fname);
+        try {
+            FileOutputStream fos = new FileOutputStream(file);
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 90, fos);
+            fos.flush();
+            fos.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        MediaScannerConnection.scanFile(this.getContext(), new String[]{file.toString()}, null,
+                new MediaScannerConnection.OnScanCompletedListener() {
+                    public void onScanCompleted(String path, Uri uri) {
+                        Log.i("ExternalStorage", "Scanned " + path + ":");
+                        Log.i("ExternalStorage", "-> uri=" + uri);
+                    }
+                });
+        Toast.makeText(this.getContext(),"Your image is saved", Toast.LENGTH_LONG).show();
 
     }
+
+    /*protected void mediaScan() {
+        this.sendBroadcast(new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE,
+                        Uri.parse(fileUri.toString())));
+    }*/
 
     private float downX, downY;
     private static final float TOUCH_TOL = 4;
@@ -361,87 +287,5 @@ public class DrawView extends View {
         return true;
     }
 
-
-
-    /**
-     * Source and explanation
-     * http://stackoverflow.com/questions/12266899/onmeasure-custom-view-explanation
-     */
-  /*  @Override
-    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-
-        int desiredWidth = 100;
-        int desiredHeight = 100;
-
-        int widthMode = MeasureSpec.getMode(widthMeasureSpec);
-        int widthSize = MeasureSpec.getSize(widthMeasureSpec);
-        int heightMode = MeasureSpec.getMode(heightMeasureSpec);
-        int heightSize = MeasureSpec.getSize(heightMeasureSpec);
-
-        int width;
-        int height;
-
-        if (widthMode == MeasureSpec.EXACTLY) {
-            width = widthSize;
-        } else if (widthMode == MeasureSpec.AT_MOST) {
-            width = Math.min(desiredWidth, widthSize);
-        } else {
-            width = desiredWidth;
-        }
-
-        if (heightMode == MeasureSpec.EXACTLY) {
-            height = heightSize;
-        } else if (heightMode == MeasureSpec.AT_MOST) {
-            height = Math.min(desiredHeight, heightSize);
-        } else {
-            height = desiredHeight;
-        }
-
-        setMeasuredDimension(width, height);
-    }*/
-
-   /* public void start() {
-        if (timer == null) {
-            Random r = new Random();
-
-            TimerTask task = new TimerTask() {
-                @Override
-                public void run() {
-                    if (backgroundPaint.getColor() == Color.RED) {
-                        backgroundPaint.setColor(Color.BLUE);
-                    } else {
-                        backgroundPaint.setColor(Color.RED);
-                    }
-                    postInvalidate();
-                }
-            };
-
-            timer = new Timer();
-            timer.schedule(task, 0, r.nextInt(2000) + 1000);
-        }
-    }*/
-
-  /*  public void stop() {
-        if (timer != null) {
-            timer.cancel();
-            timer.purge();
-            timer = null;
-        }
-    }*/
-
-  class Rectangle{
-
-      int x, y;
-      float pixelSize;
-      //float pixelSize, xStep
-
-      Rectangle(int rectangleSizeDip, int rectangleStepDip){
-
-          DisplayMetrics dm = getResources().getDisplayMetrics();
-          pixelSize = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, rectangleSizeDip, dm);
-
-          x = random.nextInt((int) (currentWidth - pixelSize));
-          y = random.nextInt((int) (currentHeight - pixelSize));
-      }
-  }
 }
+
